0b0a16de3fdf6b1b304658acdfe9f6118339c3c6
Adam Fletcher <adam@bit.io>
2021-09-09 11:49:07 -0700

    fix tabs

--- bitdotio-example/Program.cs
+++ bitdotio-example/Program.cs
@@ -29,22 +29,22 @@ namespace bitdotio_example
             using var cmd = new NpgsqlCommand(sql, con);
 
             using NpgsqlDataReader reader = cmd.ExecuteReader();
-						DataTable schemaTable = reader.GetSchemaTable();
+            DataTable schemaTable = reader.GetSchemaTable();
 
             // Show schema details
-						foreach (DataRow row in schemaTable.Rows)
-						{
-								foreach (DataColumn column in schemaTable.Columns)
-								{
-										Console.WriteLine(String.Format("{0} = {1}",
-										column.ColumnName, row[column]));
-								}
-						}
+            foreach (DataRow row in schemaTable.Rows)
+            {
+                foreach (DataColumn column in schemaTable.Columns)
+                {
+                    Console.WriteLine(String.Format("{0} = {1}",
+                    column.ColumnName, row[column]));
+                }
+            }
 
-						// Show all data
+            // Show all data
             while (reader.Read()) 
             {
-								for (int colNum = 0; colNum < reader.FieldCount; colNum++) 
+                for (int colNum = 0; colNum < reader.FieldCount; colNum++) 
                 {
                     Console.Write(reader.GetName(colNum) + "=" +  reader[colNum] + " ");
                 }
